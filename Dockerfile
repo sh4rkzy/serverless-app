f47b70e38db1b368d7aa8cad31a2033b789c03eb
Kaue Campos <kaueoliveirac11@gmail.com>
2024-03-11 00:29:52 -0300

    init: initial serverless service

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM node:alpine AS development
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+RUN npm run test
+
+RUN npm run build
+
+FROM node:alpine AS production
+
+ARG NODE_ENV=production
+
+ENV NODE_ENV=${NODE_ENV}
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install --omit=dev
+
+COPY . .
+
+COPY --from=development /usr/src/app/dist ./dist
+
+CMD ["sls", "offline", "start", "--host", "0.0.0.0"]
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './module/app/app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Handler, Context, Callback } from 'aws-lambda';
+
+let server: Handler;
+
+async function createServer(): Promise<Handler> {
+    const app = await NestFactory.create(AppModule);
+    await app.init();
+
+    const expressApp = app.getHttpAdapter().getInstance();
+
+    return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+    event: any,
+    context: Context,
+    callback: Callback
+) => {
+    server = server ?? await createServer();
+    return server(event, context, callback);
+}
